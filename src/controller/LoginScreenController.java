30d0258940046305358e47feed8c2b4e9ee94a92
Evan Bretl <evanbretl4@gmail.com>
2016-09-22 18:35:53 -0400

    Added controllers to fxml, including empty login screen controller

---
+++ src/controller/LoginScreenController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginScreenController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void authenticateLogin(ActionEvent e) {
+
+    }
+
+}
