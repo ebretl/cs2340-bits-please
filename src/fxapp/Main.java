da95443b86291b3c870057b5f44921d8f6557286
Evan Kaplan <ekaplan6@gatech.edu>
2016-09-21 23:32:09 -0400

    Made Some minor edit
    Doesn’t run well but I am now able to run it from my computer. Still
    needs major edits to get it to work. Previously getting weird errors.

--- src/fxapp/Main.java
+++ src/fxapp/Main.java
@@ -1,23 +1,34 @@
-package sample;
+package fxapp;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            StackPane page = (StackPane) FXMLLoader.load(Main.class.getResource("../view/WelcomeScreen.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Title");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
