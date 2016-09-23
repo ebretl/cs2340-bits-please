118848447fe014556e89a79f692077b123a3e769
Frances Tsenn <ftsenn@gmail.com>
2016-09-23 18:19:06 -0400

    Implemented cancel option.

--- src/controller/LoginScreenController.java
+++ src/controller/LoginScreenController.java
@@ -3,10 +3,14 @@ package controller;
 import fxapp.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.User;
+
+import java.io.IOException;
 
 public class LoginScreenController {
 
@@ -44,11 +48,23 @@ public class LoginScreenController {
         }
     }
 
+    //will be in registration screen
     void repeatUserAlert() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Invalid Username");
-        alert.setContentText("That username is already in use");
+        alert.setContentText("That username is already in use.");
         alert.showAndWait();
+    }
+
+    @FXML
+    void cancelButtonAction(ActionEvent event) throws IOException {
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        try {
+            mainApplication.showWelcomeScreen(window);
+        } catch(Exception e) {
+            //todo better logging
+            System.err.println(e.getMessage());
+        }
     }
 
 }
