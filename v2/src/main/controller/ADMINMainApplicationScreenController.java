c55fefc8fe3ed190eb792c407ddf03a62b45be55
adalmia3 <adalmia3@gatech.edu>
2016-10-07 02:01:38 -0400

    Delete Reports
    Extra credit feature added to allow managers to delete Water Reports and
    Quality Reports

--- v2/src/main/controller/ADMINMainApplicationScreenController.java
+++ v2/src/main/controller/ADMINMainApplicationScreenController.java
@@ -36,70 +36,26 @@ public class ADMINMainApplicationScreenController {
     }
 
     @FXML
-    private void submitWaterReportPressed() {
-        if (currentUser.get_ban() == 0) {
-            mainFXApplication.showSubmitWaterReportScreen();
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.initOwner(mainFXApplication.getStage());
-            alert.setTitle("Error!");
-            alert.setHeaderText("You have been banned from submitting water reports!");
-            alert.setContentText("Kindly talk to an administrator if you think this has been done in error!");
-            alert.showAndWait();
-        }
+    private void banPressed() {
     }
 
     @FXML
-    private void viewWaterReportPressed() {
-        mainFXApplication.showViewWaterReportScreen();
+    private void unbanPressed() {
+
     }
 
     @FXML
-    private void viewMapPressed() {
-        mainFXApplication.showViewMapScreen();
+    private void blockPressed() {
+
     }
 
     @FXML
-    private void submitQualityReportPressed() {
-        if (currentUser.get_type().equals(UserTypeEnum.WORKER.toString()) ||currentUser.get_type().equals(UserTypeEnum.MANAGER.toString())) {
-            mainFXApplication.showSubmitQualityReportScreen();
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.initOwner(mainFXApplication.getStage());
-            alert.setTitle("Error!");
-            alert.setHeaderText("Only Managers and Wokers can submit a Quality Report!");
-            alert.setContentText("Kindly talk to an administrator if you think this has been done in error!");
-            alert.showAndWait();
-        }
+    private void unblockPressed() {
+
     }
 
     @FXML
-    private void viewQualityReportPressed() {
-        if (currentUser.get_type().equals(UserTypeEnum.MANAGER.toString())) {
-            mainFXApplication.showViewQualityReportScreen();
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.initOwner(mainFXApplication.getStage());
-            alert.setTitle("Error!");
-            alert.setHeaderText("Only Managers can main.view Quality Reports!");
-            alert.setContentText("Kindly talk to an administrator if you think this has been done in error!");
-            alert.showAndWait();
-        }
-    }
-
-
-    @FXML
-    private void historicalGraphPressed() {
-        if (currentUser.get_type().equals(UserTypeEnum.MANAGER.toString())) {
-            mainFXApplication.showGraphParameterScreen();
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.initOwner(mainFXApplication.getStage());
-            alert.setTitle("Error!");
-            alert.setHeaderText("Only Managers can main.view Historical Graphs!");
-            alert.setContentText("Kindly talk to an administrator if you think this has been done in error!");
-            alert.showAndWait();
-        }
+    private void deleteAccountPressed() {
 
     }
 }
